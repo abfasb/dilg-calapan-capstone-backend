0acbfcd86d7a104966719ed391556e98aaff653d
mtthw <matbalinton@gmail.com>
2025-02-24 14:54:11 +0800

    updateController

--- src/routes/ReportRoutes.ts
+++ src/routes/ReportRoutes.ts
@@ -1,8 +1,10 @@
 import Express from 'express';
-import { createReport } from '../controllers/reportController';
+import { createReport, getReportForms, updateReportForms } from '../controllers/reportController';
 
 const router = Express.Router();
 
 router.post('/create-report', createReport);
+router.get('/get-report', getReportForms);
+router.put('/update-report/:id', updateReportForms);
 
 export default router;
