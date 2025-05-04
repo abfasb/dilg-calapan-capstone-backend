61ad89af3c7916043ab500fc48c6795aa255c60c
mtthw <matbalinton@gmail.com>
2025-05-04 13:32:20 +0800

    update profile controler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -294,3 +294,40 @@ export const getLGUUsers = async (req : Request, res : Response, next: NextFunct
       res.status(500).json({ message: 'Error fetching LGU users', error });
     }
   };
+
+  export const updateProfile = async (req: Request, res: Response) : Promise<void>=> {
+    try {
+      if (!req.user) {
+         res.status(401).json({ message: 'Unauthorized: User not authenticated' });
+         return
+      }
+      const userId = (req.user as any)._id;
+      const { firstName, lastName, phoneNumber, email } = req.body;
+  
+      const user = await User.findById(userId);
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+  
+      if (email !== user.email) {
+        res.status(400).json({ message: 'Email cannot be changed' });
+        return;
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+          firstName,
+          lastName,
+          phoneNumber
+        },
+        { new: true, runValidators: true }
+      ).select('-password -resetToken -resetTokenExpiry');
+  
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
