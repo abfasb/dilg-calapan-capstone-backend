51e2b8b18d6514e82537a0f505250188972726b3
mtthw <matbalinton@gmail.com>
2025-08-25 19:11:37 +0800

    add that new cors

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ const io = new Server(server, {
 });
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: process.env.CLIENT_URL, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
 };
